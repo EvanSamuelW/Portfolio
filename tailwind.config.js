e0930f11fc20b718a173d6865778efd34f3d091d
EvanSamuelW <70187319+EvanSamuelW@users.noreply.github.com>
2022-05-20 13:47:23 +0700

    first_commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#3d4c32',
+                yellow: '#D4D2A5',
+                third: '#134074'
+            }
+        },
+    },
+    plugins: [require('flowbite/plugin')],
+}
